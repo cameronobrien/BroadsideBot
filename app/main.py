9c05f3c7e15a4197efb6493adc59dcf77839fda1
Cameron O'Brien <piggiescanfly238@gmail.com>
2017-04-11 16:41:24 -0500

    EVE Implant Set price checks now implemented

--- app/main.py
+++ app/main.py
@@ -170,4 +170,70 @@ async def getprice(msg):
         return await my_bot.say("Unable to find search item")
 
 
+@my_bot.command()
+async def getsetprice(msg):
+    EVECENTRAL = "http://api.eve-central.com/api/marketstat?typeid=%s&regionlimit=10000002"
+    EVESTATICDATADUMP = "data/sqlite-latest.sqlite"
+
+    if os.path.isfile(os.path.expanduser(EVESTATICDATADUMP)):
+        conn = sqlite3.connect(os.path.expanduser(EVESTATICDATADUMP))
+    else:
+        conn = None
+
+    def get_type_id(name):
+        c = conn.cursor()
+        c.execute("select typeName, typeID from invTypes where typeName = '{0}%' collate nocase;".format(name))
+        result = c.fetchone()
+        if result:
+            return result
+        c.execute("select typeName, typeID from invTypes where typeName like '%{0}%' collate nocase;".format(name))
+        results = c.fetchall()
+        if len(results) == 0:
+            return None
+        results = sorted(results, key=lambda x: len(x[0]))
+        print(results[0])
+        return results[0]
+
+    def item_to_price(item):
+        try:
+            result = get_type_id(item)
+            assert result
+            item = result[0]
+            url = EVECENTRAL % result[1]
+            print(url)
+            soup = BS(urllib.request.urlopen(url), "html.parser")
+            price = str(soup.find("sell").min)
+            removetags = re.compile("<(.|\n)*?>")
+            price = removetags.sub("", price)
+            price = float(price)
+            if price == 0:
+                raise ZeroDivisionError
+            import locale
+            locale.setlocale(locale.LC_ALL, "")
+            formatted_price = locale.format('%d', price, True)
+            return item, formatted_price, price
+        except Exception as e:
+            return None, None, None
+
+    if not conn:
+        return await my_bot.say("EVE static data dump not loaded")
+    types = ["alpha", "beta", "gamma", "delta", "epsilon", "omega"]
+    term = msg.replace("!getsetprice ", "")
+    term = term.strip()
+    terms = map(lambda x: term + " " + x, types)
+    total = 0
+    results = []
+    for item in terms:
+        r = item_to_price(item)
+        print(item, r)
+        if r[0] and r[1]:
+            results.append((r[0], r[1]))
+            total = total + r[2]
+    import locale
+    locale.setlocale(locale.LC_ALL, "")
+    ftotal = locale.format('%d', total, True)
+    results.append(("total", ftotal))
+    return await my_bot.say("%s" % str(results))
+
+
 my_bot.run(CLIENT_ID)
